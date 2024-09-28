7ae35f5eabe94fb0aa6969d4f51a4819dfc5a492
Juanfelon14x <juanfelon14x@gmail.com>
2024-09-28 14:46:22 -0500

    Update Biblioteca.java

--- Biblioteca.java
+++ Biblioteca.java
@@ -1,15 +1,16 @@
 package co.edu.uniquindio.poo;
 
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.LinkedList;
 
 public class Biblioteca {
 
     private String nombre;
-    private Collection<Estudiante> estudiantes;
-    private Collection<Bibliotecario> bibliotecarios;
-    private Collection<Prestamo> prestamos;
-    private Collection<Libro> libros;
+    private LinkedList<Estudiante> estudiantes;
+    private LinkedList<Bibliotecario> bibliotecarios;
+    private LinkedList<Prestamo> prestamos;
+    private LinkedList<Libro> libros;
     private double ganancia;
     private int cantidadLibros;
 
@@ -35,31 +36,31 @@ public class Biblioteca {
         return estudiantes;
     }
 
-    public void setEstudiantes(Collection<Estudiante> estudiantes) {
+    public void setEstudiantes(LinkedList<Estudiante> estudiantes) {
         this.estudiantes = estudiantes;
     }
 
-    public Collection<Bibliotecario> getBibliotecarios() {
+    public LinkedList<Bibliotecario> getBibliotecarios() {
         return bibliotecarios;
     }
 
-    public void setBibliotecarios(Collection<Bibliotecario> bibliotecarios) {
+    public void setBibliotecarios(LinkedList<Bibliotecario> bibliotecarios) {
         this.bibliotecarios = bibliotecarios;
     }
 
-    public Collection<Prestamo> getPrestamos() {
+    public LinkedList<Prestamo> getPrestamos() {
         return prestamos;
     }
 
-    public void setPrestamos(Collection<Prestamo> prestamos) {
+    public void setPrestamos(LinkedList<Prestamo> prestamos) {
         this.prestamos = prestamos;
     }
 
-    public Collection<Libro> getLibros() {
+    public LinkedList<Libro> getLibros() {
         return libros;
     }
 
-    public void setLibros(Collection<Libro> libros) {
+    public void setLibros(LinkedList<Libro> libros) {
         this.libros = libros;
     }
 
@@ -81,7 +82,7 @@ public class Biblioteca {
 
     @Override
     public String toString() {
-        return "Biblioteca [nombre=" + nombre + ", estudiantes=" + estudiantes + ", bibliotecarios=" + bibliotecarios
+        return "\n\nBiblioteca [nombre=" + nombre + ", estudiantes=" + estudiantes + ", bibliotecarios=" + bibliotecarios
                 + ", prestamos=" + prestamos + ", libros=" + libros + ", ganancia=" + ganancia + ", cantidadLibros="
                 + cantidadLibros + "]";
     }
@@ -100,18 +101,57 @@ public class Biblioteca {
     public void agregarLibro(Libro libro) {
         if (verificarLibro(libro.getCodigo()) == false) {
             libros.add(libro);
+            setCantidadLibros(+1);
         }
     }
     public void eliminarLibro(Libro libro) {
         if (verificarLibro(libro.getCodigo()) == true) {
             libros.remove(libro);
+            setCantidadLibros(-1);
         }
     }
 //----------------------------------Gestion parcial-----------------------------------//
-
+    /*
+    * 3.2
+    * Mostrar los datos de un libro dado su codigo
+    */
     public void mostrardatoslibro(String codigoelegido){
-
+        String mensaje = "";
+        for (Libro libro : libros) {
+            if (libro.getCodigo().equals(codigoelegido)) {
+                mensaje = libro.toString();
+                break;
+            }
+        }
+        System.out.println(mensaje);
     }
+
+    /*
+    * 3.3
+    * Mostrar el numero de prestamos en los que está involucrado un libro dado su titulo
+    */
+    public void mostrarprestamoslibro(String tituloelegido){
+        int contador = 0;
+        for (Prestamo prestamo : prestamos) {
+            for (DetallePrestamo detallePrestamo : prestamo.getDetallePrestamos()) {
+                if (detallePrestamo.getLibro().getTitulo().equals(tituloelegido)) {
+                    contador++;
+                }
+            }
+
+        }
+        System.out.println(contador);
+    }
+    /**
+     * 3.4
+     * Metodo alternativo para agregar un libro, sobreescribiendolo en la posicion de otro
+     * @param libronuevo un libro que no esté en la base de datos de la biblioteca
+     * @param libroacambiar un libro existente a eliminar
+     */
+    public void reemplazarLibro(Libro libronuevo, Libro libroacambiar){
+        libros.set(libros.indexOf(libroacambiar), libronuevo);
+    }
+
 //--------------------------------------------------Estudiante-----------------------------------------------------//
 
     public boolean verificarEstudiante(String cedula) {
@@ -149,7 +189,7 @@ public class Biblioteca {
             bibliotecarios.add(bibliotecario);
         }
     }
-    public void eliminarEstudiante(Bibliotecario bibliotecario) {
+    public void eliminarBibliotecario(Bibliotecario bibliotecario) {
         if (verificarLibro(bibliotecario.getCedula()) == true) {
             bibliotecarios.remove(bibliotecario);
         }
@@ -165,18 +205,124 @@ public class Biblioteca {
         }
         return centinela;
     }
-    public void agregarPrestamo(Prestamo prestamo, Estudiante estudiante, Bibliotecario bibliotecario) {
-        if (verificarPrestamo(prestamo.getCodigo()) == false) {
-            prestamos.add(prestamo);
-            estudiante.agregarPrestamo(prestamo);
-            bibliotecario.agregarPrestamo(prestamo);
+    /*
+     * Metodo que, ya habiendo creado un prestamo, hace efectiva la agregacion del estudiante y 
+     */
+    public void agregarPrestamoBiblioteca(Prestamo prestamonuevo) {
+        if (verificarPrestamo(prestamonuevo.getCodigo()) == false) {
+            prestamonuevo.getEstudiante().agregarPrestamoEstudiante(prestamonuevo);
+            prestamonuevo.getBibliotecario().agregarPrestamoBibliotecario(prestamonuevo);
+            agregarEstudiante(prestamonuevo.getEstudiante());
+            agregarBibliotecario(prestamonuevo.getBibliotecario());
+            prestamos.add(prestamonuevo);
         }
     }
-    public void eliminarPrestamo(Prestamo prestamo, Estudiante estudiante, Bibliotecario bibliotecario) {
+    public void eliminarPrestamoBiblioteca(Prestamo prestamo) {
         if (verificarPrestamo(prestamo.getCodigo()) == true) {
             prestamos.remove(prestamo);
-            estudiante.eliminarPrestamo(prestamo);
-            bibliotecario.eliminarPrestamo(prestamo);
+            prestamo.getEstudiante().eliminarPrestamoEstudiante(prestamo);
+            prestamo.getBibliotecario().eliminarPrestamoBibliotecario(prestamo);
+            eliminarEstudiante(prestamo.getEstudiante());
+            eliminarBibliotecario(prestamo.getBibliotecario());
         }
+    }
+    /*
+     * Metodo para entregar un prestamo, es decir, elminarlo de todas las listas en donde esté,
+     * actualizar el subtotal y sumarlo a las ganancias de la biblioteca
+     */
+    public void entregarPrestamo(Prestamo prestamobuscado){
+        int total = 0;
+        for (DetallePrestamo detallePrestamo : prestamobuscado.getDetallePrestamos()) {
+            prestamobuscado.actualizarSubtotal(detallePrestamo);
+            total += getGanancia();
+        }
+        setGanancia(getGanancia() + total);
+        prestamos.remove(prestamobuscado);
+        prestamobuscado.getEstudiante().eliminarPrestamoEstudiante(prestamobuscado);
+        prestamobuscado.getBibliotecario().eliminarPrestamoBibliotecario(prestamobuscado);
+    }
+    public void mostrardatosPrestamo(String codigoelegido){
+        String mensaje = "";
+        for (Prestamo prestamo : prestamos) {
+            if (prestamo.getCodigo().equals(codigoelegido)) {
+                mensaje = prestamo.toString();
+                break;
+            }
+        }
+        System.out.println(mensaje);
+    }
+    public void mostrarprestamosBibliotecarios(){
+        String prestamos = "Los empleados y sos respectivos prestamos son:" + "\n";
+        for(Bibliotecario bibliotecario : bibliotecarios){
+            prestamos += bibliotecario.getNombre() + " = " + bibliotecario.getPrestamos().size() + "\n";
+        }
+        System.out.println(prestamos);
+    }
+    public void estudianteconmasprestamos(){
+        int prestamomayor = 0;
+        Estudiante estudiantemayor = null;
+        for (Estudiante estudiante : estudiantes) {
+            int contador = 0;
+            contador = estudiante.getPrestamos().size();
+
+            if (contador > prestamomayor) {
+                estudiantemayor = estudiante;
+                prestamomayor = contador;
+            }
+        }
+        System.out.println("el estudiante" + estudiantemayor + "hizo" + prestamomayor + "prestamos");    
+    }
+    public void dineroapagarbibliotecarios(){
+        double dineroapagar = 0;
+        double bonificacion1 = 0;
+        double bonificacion2 = 0;
+        for (Bibliotecario bibliotecario : bibliotecarios) {
+            for (Prestamo prestamo : bibliotecario.getPrestamos()) {
+                for (DetallePrestamo detallePrestamo : prestamo.getDetallePrestamos()) {
+                    prestamo.actualizarSubtotal(detallePrestamo);
+                    bonificacion1 = 0.2 * detallePrestamo.getSubTotal();
+                    bonificacion2 = bonificacion1 + (0.02 * bibliotecario.getAntiguedad());
+                    dineroapagar += bonificacion2;
+                }
+            }
+        }
+        System.out.println("El dinero total a pagar a todos los bibliotecarios es" + dineroapagar);
+    }
+
+
+    public static void main(String[] args) {
+
+        //-----------------------------------------Crearvariables----------------------------------------//
+
+        Biblioteca biblioteca1 = new Biblioteca("Libros libreros", 0, 0);
+
+        Estudiante estudiante1 = new Estudiante("Richard", "2354345",
+        "Rich43@gmail.com", 35445654, 21);
+
+        Bibliotecario bibliotecario1 = new Bibliotecario("Esteban", "3654534",
+        "Est2423@gmail.com",435464576, 500000, 35);
+
+            LocalDate fechapublicacionlibro1 = LocalDate.of(2016, 4, 5);
+
+        Libro libro1 = new Libro("3546436", "456456", "Ramiro del valle", 
+        "Mundopapel", "Libro1", fechapublicacionlibro1, true,
+        40);
+
+            LocalDate fechaprestamo1 = LocalDate.of(2024,10,16);
+            LocalDate fechaentrega1 = LocalDate.of(2024,11,16);
+
+
+        Prestamo prestamo1 = new Prestamo(fechaprestamo1, fechaentrega1,
+        50, "546476", biblioteca1, estudiante1, bibliotecario1);
+        
+        DetallePrestamo detallePrestamo1 = new DetallePrestamo(3, libro1);
+
+        //--------------------------------------------Gestionar-------------------------------------------//
+
+        biblioteca1.agregarPrestamoBiblioteca(prestamo1);
+        prestamo1.agregarDetallePrestamo(detallePrestamo1);
+
+        
+        System.out.println(biblioteca1);
     }
 }
